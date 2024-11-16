c1bdb8f40fc1f9afce4c272f30659bf8675d3910
Chris Free <chris@chrisfree.com>
2024-11-16 09:20:16 -0600

    Initial Commit

---
+++ geoJSONEditorTests/geoJSONEditorTests.swift
@@ -0,0 +1,17 @@
+//
+//  geoJSONEditorTests.swift
+//  geoJSONEditorTests
+//
+//  Created by Christopher Free on 11/16/24.
+//
+
+import Testing
+@testable import geoJSONEditor
+
+struct geoJSONEditorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
