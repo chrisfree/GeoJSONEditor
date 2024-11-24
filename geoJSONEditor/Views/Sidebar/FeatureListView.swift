86bcabe9be2351b2a89f9b4c81ec5d2e7381c020
Chris Free <chris@chrisfree.com>
2024-11-24 12:47:15 -0600

    Add ability to reorder features.

--- geoJSONEditor/Views/Sidebar/FeatureListView.swift
+++ geoJSONEditor/Views/Sidebar/FeatureListView.swift
@@ -10,20 +10,28 @@ import SwiftUI
 struct FeatureListView: View {
     @Binding var selectedFeatures: Set<UUID>
     @Binding var layers: [LayerState]
-    @Binding var editingState: EditingState  // Changed to @Binding
+    @Binding var editingState: EditingState
 
     var body: some View {
         List(selection: $selectedFeatures) {
             ForEach(layers) { layer in
                 FeatureRowView(layer: layer,
-                               layers: $layers,
-                               editingState: $editingState)  // Pass binding
+                             layers: $layers,
+                             editingState: $editingState)
             }
+            .onMove { source, destination in
+                layers.move(fromOffsets: source, toOffset: destination)
+                // After moving, we need to update visibility based on selection
+                if let selectedId = selectedFeatures.first {
+                    updateLayerVisibility(for: selectedId)
+                }
+            }
+            .moveDisabled(false)
         }
-        .listStyle(.sidebar)  // This gives a more subtle selection highlight
-        .accentColor(.secondary) 
-        .onChange(of: selectedFeatures) { newSelection in
-            handleSelectionChange(newSelection)
+        .listStyle(.sidebar)
+        .accentColor(.secondary)
+        .onChange(of: selectedFeatures) { oldValue, newValue in
+            handleSelectionChange(newValue)
         }
     }
 
